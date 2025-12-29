7435adc337bddf5a204603414bfe3e528521b180
nhp <ricardoryn1317@gmail.com>
2025-12-29 11:28:38 +0800

    chore(config): update terminal, input method, and config files

--- README.md
+++ README.md
@@ -1,20 +1,7 @@
 ```bash
 # 文件
-
-
-
 ln -s ~/dotfiles/rime-ice/default.custom.yaml ~/.local/share/fcitx5/rime/default.custom.yaml
 ln -s ~/dotfiles/rime-ice/rime_ice.custom.yaml ~/.local/share/fcitx5/rime/rime_ice.custom.yaml
 
 # 文件夹
-ln -s ~/dotfiles/alacritty ~/.config/alacritty
-ln -s ~/dotfiles/kitty ~/.config/kitty
-ln -s ~/dotfiles/ghostty ~/.config/ghostty
-ln -s ~/dotfiles/nushell ~/.config/nushell
-ln -s ~/dotfiles/fastfetch ~/.config/fastfetch
-ln -s ~/dotfiles/bat ~/.config/bat
-ln -s ~/dotfiles/flameshot ~/.config/flameshot
-ln -s ~/dotfiles/niri ~/.config/niri
-ln -s ~/dotfiles/yazi ~/.config/yazi
-ln -s ~/dotfiles/i3status ~/.config/i3status
 ```
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,15 +1,27 @@
-local Config = require("config")
+-- local Config = require("config")
+--
+-- require("events.left-status").setup()
+-- require("events.right-status").setup({ date_format = "%a %H:%M:%S" })
+-- require("events.tab-title").setup({ hide_active_tab_unseen = false, unseen_icon = "circle" })
+-- require("events.new-tab-button").setup()
+--
+-- return Config:init()
+--   :append(require("config.appearance"))
+--   :append(require("config.bindings"))
+--   :append(require("config.domains"))
+--   :append(require("config.fonts"))
+--   :append(require("config.general"))
+--   :append(require("config.launch"))
+--   :append(require("config.layout")).options
+local wezterm = require("wezterm")
 
-require("events.left-status").setup()
-require("events.right-status").setup({ date_format = "%a %H:%M:%S" })
-require("events.tab-title").setup({ hide_active_tab_unseen = false, unseen_icon = "circle" })
-require("events.new-tab-button").setup()
+local config = {
+  default_prog = { "nu" },
+  font = wezterm.font("Maple Mono NF CN"),
+  font_size = 11,
+  color_scheme = "catppuccin-latte",
+  hide_tab_bar_if_only_one_tab = true,
+  use_fancy_tab_bar = true,
+}
 
-return Config:init()
-  :append(require("config.appearance"))
-  :append(require("config.bindings"))
-  :append(require("config.domains"))
-  :append(require("config.fonts"))
-  :append(require("config.general"))
-  :append(require("config.launch"))
-  :append(require("config.layout")).options
+return config
