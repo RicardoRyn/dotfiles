5b08955ad07fa6d19084d71f6c063abc7211d300
nhp <ricardoryn1317@gmail.com>
2026-01-14 10:54:12 +0800

    style(yazi): update git status icons with new glyphs

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,10 +1,10 @@
 th.git = th.git or {}
-th.git.modified_sign = ""
-th.git.added_sign = ""
-th.git.untracked_sign = ""
-th.git.ignored_sign = ""
-th.git.deleted_sign = ""
-th.git.updated_sign = "󰚰"
+th.git.modified_sign = "󰏭 "
+th.git.added_sign = "󰜄 "
+th.git.untracked_sign = " "
+th.git.ignored_sign = " "
+th.git.deleted_sign = "󰛲 "
+th.git.updated_sign = " "
 require("git"):setup { order = 0 }
 require("full-border"):setup()
 require("starship"):setup()
