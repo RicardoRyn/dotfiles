ad72a8c0383811229238f6f07bb9c7482a4914c6
Ricardo_T8 <ricardoryn1317@gmail.com>
2026-07-02 15:24:23 +0800

    feat(yazi): migrate to new plugins and update configurations

--- yazi/config/init.lua
+++ yazi/config/init.lua
@@ -1,11 +1,11 @@
 th.git = th.git or {}
-th.git.added_sign = ""
-th.git.modified_sign = "○"
-th.git.deleted_sign = ""
-th.git.untracked_sign = "?"
-th.git.ignored_sign = " "
-th.git.updated_sign = " "
-require("git"):setup({ order = 1500 })
+th.git.added_sign = "_A"
+th.git.modified_sign = "_M"
+th.git.deleted_sign = "_D"
+th.git.untracked_sign = "??"
+th.git.ignored_sign = "!!"
+th.git.updated_sign = "M_"
+require("git"):setup({ order = 999 })
 require("full-border"):setup()
 require("starship"):setup()
 require("toggle-pane"):entry("min-preview")
